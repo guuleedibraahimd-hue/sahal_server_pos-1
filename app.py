6fd413db726c4898135e300a5c32ac1be3a2a2ef
Moodir Manooge <moodirmanooge@gmail.com>
2026-07-14 02:07:38 +0300

    Update

--- app.py
+++ app.py
@@ -575,9 +575,10 @@ def save_supermarket_firestore(data):
 def save_order_firestore(data):
     db.collection("orders").add(data)
 
+
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("index.html")
 
 
 @app.route("/index")
